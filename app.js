77f3d3b6c5a4da95b1035d54dd8f1b402da67cf6
TekniqueJoseph <tekniquejoseph@gmail.com>
2023-07-26 19:29:12 -0400

    lesson code for factory functions

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const add = ((num1, num2) => { // Pure function. Same inputs yield the same resu
 
 // const sendDataToServer = (() => {}) // Side effect neccessary and acceptable
 
-console.log(add(1,5)) // = 6
-console.log(add(12,15)) // = 27
+// console.log(add(1,5)) // = 6
+// console.log(add(12,15)) // = 27
 
 const addRandom = ((num1) => { //Not Pure Function. Results may vary.
     return num1 + Math.random()
 })
-console.log(addRandom(5)) // = ???
+// console.log(addRandom(5)) // = ???
 
 let previousResult = 0;
 
@@ -20,7 +20,7 @@ const addMoreNumbers = ((num1, num2) => {// Side effect by changing a variable d
     return sum;
 })
 
-console.log(addMoreNumbers(1,5))
+// console.log(addMoreNumbers(1,5))
 
 const hobbies = ['Fitness', 'Meal Prep']
 
@@ -29,4 +29,21 @@ const printHobbies = ((h) => { // Side effect caused by printHobbies adding an a
     console.log(h)
 })
 
-printHobbies(hobbies);
+// printHobbies(hobbies);
+
+
+const createTaxCalculator = ((tax) => { // receives tax rate from function call 
+    // console.log(tax)
+    const calculateTax = ((amount) => { // inner function has access to outer functions parameters due to scope. Receives amount via call 
+        return amount * tax;
+    })
+    // console.log(calculateTax)
+    return calculateTax; //returns pointer to fucntion
+})
+
+const calculateVatAmount = createTaxCalculator(.19) // passes .19 to createTaxCalculator, wich reutrns the function calculateTax which is stored in calculateVatAmount
+const calculateIncomeTaxAmount = createTaxCalculator(.25)
+
+console.log(calculateVatAmount(100)) // calls caculate tax and passes an argument. caculate tax has access to the variable tax due to scope
+console.log(calculateVatAmount(200))
+
